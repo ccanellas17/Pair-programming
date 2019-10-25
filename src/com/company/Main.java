769e703d1fa0689252262986c14d6c484f99ba9f
Carlos <ccanellas.ieu2017@student.ie.edu>
2019-10-25 18:36:47 +0200

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.io.*;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        LinkedList<String> dictionary = new LinkedList<String>();
+
+        String unsorteddict = "/Users/carlos/IdeaProjects/pair-programming/unsorteddict.txt";
+
+        File f = new File(unsorteddict);
+        FileReader r = new FileReader(f);
+        BufferedReader reader = new BufferedReader(r);
+
+        }
+
+        public static void fill (LinkedList<String> dict){
+            for (int i=0; i<dict.size(); i++){
+                //We need to try to use the compareTo() function
+            }
+
+    }
+}
+l
+
+
+
+
