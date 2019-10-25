0a593ebd2b44e020661a251fca226641cdd4b62c
Carlos <ccanellas.ieu2017@student.ie.edu>
2019-10-25 19:25:09 +0200

    First attempt

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,22 +11,34 @@ public class Main {
     public static void main(String[] args) throws IOException {
         LinkedList<String> dictionary = new LinkedList<String>();
 
-        String unsorteddict = "/Users/carlos/IdeaProjects/pair-programming/unsorteddict.txt";
+        //Create access to the unsorted dictionary file
+        File f = new File("/Users/carlos/IdeaProjects/pair-programming/unsorteddict.txt");
 
-        File f = new File(unsorteddict);
+        //Create FileReader to read the file
         FileReader r = new FileReader(f);
+
         BufferedReader reader = new BufferedReader(r);
+
+        //Create a FileWriter for the sorted dictionary
+        FileWriter writer = new FileWriter("C:\\Users\\Marlene\\Workspace\\IHATELIFE\\src\\com\\company\\testOutput.txt");
 
         }
 
+        //Create fill() method to fill words in alphabetical order
         public static void fill (LinkedList<String> dict){
-            for (int i=0; i<dict.size(); i++){
-                //We need to try to use the compareTo() function
+            for (int i = 0; i < dict.size(); i++) {
+                for (int j = i + 1; j < dict.size(); i++) {
+                    if (dict.get(i).compareTo(dict.get(j)) > 0) {
+                        String word = dict.get(j);
+                        dict.set(j, dict.get(i));
+                        dict.set(i, word);
+                    }
+                }
             }
+        }
 
     }
-}
-l
+
 
 
 
